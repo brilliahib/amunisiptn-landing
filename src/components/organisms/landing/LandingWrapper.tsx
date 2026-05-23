d5cf2e33b32af7d6390898d63ad55ef4220bd774
brilliahib <brilliahib21@gmail.com>
2026-05-23 22:57:15 +0700

    fix: remove section landing ticket in landing page

--- src/components/organisms/landing/LandingWrapper.tsx
+++ src/components/organisms/landing/LandingWrapper.tsx
@@ -1,6 +1,5 @@
 import LandingHero from "./LandingHero";
 import LandingPTN from "./LandingPTN";
-import LandingTicket from "./LandingTicket";
 
 export default function LandingWrapper() {
   return (
@@ -9,7 +8,6 @@ export default function LandingWrapper() {
         <LandingHero />
         <LandingPTN />
       </div>
-      <LandingTicket />
     </section>
   );
 }
